9a82994de9a174963a365609bfa21cf14914beb5
DeroMal <derrickmal123@gmail.com>
2024-09-25 15:31:17 +0300

    fixed path

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,7 +1,7 @@
 // src/routes/PrivateRoute.js
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../context/AuthContext';
 
 const PrivateRoute = ({ element }) => {
     const { isAuthenticated } = useAuth();
